c63de1f452a66b91336ed214f49571f2771d7432
Thit Lwin <saithitlwin@gmail.com>
2020-01-03 14:02:17 +0630

    add lat/long copier

--- app/src/main/java/com/example/latlongtaker/MapsActivity.java
+++ app/src/main/java/com/example/latlongtaker/MapsActivity.java
@@ -1,17 +1,22 @@
 package com.example.latlongtaker;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -23,7 +28,6 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -33,6 +37,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PointOfInterest;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Locale;
@@ -47,17 +52,27 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleMap mMap;
     LatLng latLong;
     FusedLocationProviderClient mFusedLocationClient;
-    TextInputEditText edLatitude, edLongitude;
+//    TextInputEditText edLatitude, edLongitude;
     SupportMapFragment mapFragment;
     LocationCallback mLocationCallback;
+    LocationManager locationManager;
+    View contentView;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkGps();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        edLatitude = findViewById(R.id.edit_text_latitude);
-        edLongitude = findViewById(R.id.edit_text_longitude);
+        contentView = findViewById(R.id.contentView);
+
+//        edLatitude = findViewById(R.id.edit_text_latitude);
+//        edLongitude = findViewById(R.id.edit_text_longitude);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
@@ -70,12 +85,41 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             public void onLocationResult(LocationResult locationResult) {
                 super.onLocationResult(locationResult);
 
-                Toast.makeText(MapsActivity.this, "Updated location.", Toast.LENGTH_SHORT).show();
-                showLocation(locationResult.getLastLocation());
+                latLong = new LatLng(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude());
+                mapFragment.getMapAsync(MapsActivity.this);
+//                Toast.makeText(MapsActivity.this, "Updated location.", Toast.LENGTH_SHORT).show();
+//                showLocation(locationResult.getLastLocation());
             }
         };
 
         getLocation();
+    }
+
+    private void checkGps() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            displayAlertDialogForGps();
+        }
+    }
+
+    private void displayAlertDialogForGps() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("GPS setting seems disabled, please open it.")
+                .setCancelable(false)
+                .setPositiveButton("Open", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .create();
+        builder.show();
     }
 
     @Override
@@ -101,27 +145,22 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     REQUEST_LOCATION_PERMISSION);
         } else {
 
+            // update device last know location
             mFusedLocationClient.requestLocationUpdates(getLocationRequest(), mLocationCallback, null);
 
-            /*mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            // display device last know location
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
                 @Override
                 public void onSuccess(Location location) {
                     if (location != null) {
-                        showLocation(location);
+                        latLong = new LatLng(location.getLatitude(), location.getLongitude());
+                        mapFragment.getMapAsync(MapsActivity.this);
                     }
                 }
-            });*/
+            });
         }
     }
 
-    private void showLocation(Location location) {
-
-        latLong = new LatLng(location.getLatitude(), location.getLongitude());
-        mapFragment.getMapAsync(this);
-
-        edLatitude.setText(location.getLatitude() + "");
-        edLongitude.setText(location.getLongitude() + "");
-    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -149,22 +188,41 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
+
+        enableMyLocation();
 //        mMap.addMarker(new MarkerOptions().position(latLong).title("Marker in Sydney"));
-        mMap.addMarker(new MarkerOptions().position(latLong));
+//        mMap.addMarker(new MarkerOptions().position(latLong));
+
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLong));
 //        mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLong, zoom));
+
         setMapLongClick(mMap);
 
         setPoiClick(mMap);
 
-        enableMyLocation();
+        setOnMarkerClick(mMap);
     }
 
-    public void copyLatitude(View view) {
+    private void setOnMarkerClick(GoogleMap mMap) {
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Log.d(TAG, "Marker click --=="+marker.getTitle());
+                marker.showInfoWindow();
+                showSnapBar(marker.getTag().toString());
+                return true;
+            }
+        });
+    }
+
+    /*public void copyLatitude(View view) {
 
         if (TextUtils.isEmpty(edLatitude.getText())) {
             Toast.makeText(this, "Empty latitude", Toast.LENGTH_SHORT).show();
@@ -187,7 +245,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         clipboardManager.setPrimaryClip(clipData);
         Toast.makeText(this, "Copied longitude", Toast.LENGTH_SHORT).show();
     }
-
+*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -215,27 +273,58 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     public void setMapLongClick(final GoogleMap map) {
+
         map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
+
                 String snipped = String.format(Locale.getDefault(),
-                        "Lat: %1$.5f, Long: %2$.5f", latLng.latitude, latLng.longitude);
-                map.addMarker(new MarkerOptions().position(latLng)
-                        .title("Dropped Pin").snippet(snipped));
+                        "Lat: %1$.6f, Long: %2$.6f", latLng.latitude, latLng.longitude);
+                Marker marker = map.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title("Your Dropped Pin")
+                        .snippet(snipped));
+//                marker.showInfoWindow();
+                marker.setTag(snipped);
             }
         });
     }
 
     public void setPoiClick(final GoogleMap map) {
+
         map.setOnPoiClickListener(new GoogleMap.OnPoiClickListener() {
             @Override
             public void onPoiClick(PointOfInterest pointOfInterest) {
+
+                LatLng latLng = pointOfInterest.latLng;
+                String snipped = String.format(Locale.getDefault(),
+                        "Lat: %1$.6f, Long: %2$.6f", latLng.latitude, latLng.longitude);
+
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
                 Marker poiMarker = map.addMarker(new MarkerOptions()
-                .position(pointOfInterest.latLng)
-                .title(pointOfInterest.name));
-                poiMarker.showInfoWindow();
+                .position(latLng)
+                .title(pointOfInterest.name)
+                .snippet(snipped));
+//                poiMarker.showInfoWindow();
+                poiMarker.setTag(snipped);
             }
         });
+
+    }
+
+    private void showSnapBar(final String msg) {
+        Log.d(TAG, "show shap bar");
+//        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        Snackbar.make(contentView, msg, Snackbar.LENGTH_LONG)
+                .setAction("Copy", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                        ClipData clipData = ClipData.newPlainText("location", msg);
+                        clipboardManager.setPrimaryClip(clipData);
+                    }
+                }).show();
     }
 
     public void enableMyLocation() {
